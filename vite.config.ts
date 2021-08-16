bfe919f4df30001f90335fc451d67980b5bfb444
takuoaoi <>
2021-08-16 18:25:27 +0900

    modified base option for publish

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), VitePWA()]
+  plugins: [reactRefresh(), VitePWA()],
+  base: './'
 })
